8fae4295a2dc4c1ac78bd853b6b4ea3b2b838089
gyfrans <gyfrans.co@gmail.com>
2026-08-12 10:23:17 +0700

    feat(database): add user profile schema

--- database/migrations/2026_08_08_043735_create_profiles_table.php
+++ database/migrations/2026_08_08_043735_create_profiles_table.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
@@ -35,9 +33,7 @@ return new class extends Migration
         ');
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('profiles');
--- database/migrations/2026_08_08_043747_create_student_profiles_table.php
+++ database/migrations/2026_08_08_043747_create_student_profiles_table.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('student_profiles', function (Blueprint $table) {
@@ -27,9 +25,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('student_profiles');
