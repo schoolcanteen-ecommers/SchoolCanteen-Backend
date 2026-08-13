8e91715f40d4dbadfd8dd3de045fd89bd856c3c0
gyfrans <gyfrans.co@gmail.com>
2026-08-13 11:24:00 +0700

    feat(products): tambah api dan models

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Student\OrderController;
+use App\Http\Controllers\Api\V1\Student\WalletController;
 use Illuminate\Support\Facades\Route;
+
+
+/* Health Check */
 
 Route::get('/health', function () {
     return response()->json([
@@ -8,4 +13,23 @@ Route::get('/health', function () {
         'message' => 'SchoolCanteen API is running',
         'version' => 'v1',
     ]);
+});
+
+
+/* API Version 1 */
+
+Route::prefix('v1')->group(function () {
+
+    /* Public API */
+
+    Route::get('/products', [
+        ProductController::class,
+        'index',
+    ]);
+
+    Route::get('/products/{product}', [
+        ProductController::class,
+        'show',
+    ]);
+
 });
